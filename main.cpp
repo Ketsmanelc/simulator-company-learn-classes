97d0ee0ac850cc5b9ebe6f6d82b1d2d8d168bf1a
Ketsman Aleksandr <ketsman94@mail.ru>
2022-01-22 22:07:11 +0300

    add Head and Team classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 enum Tasks
 {
@@ -28,32 +29,73 @@ public:
     }
 };
 
+class Team
+{
+    //Worker* workers = nullptr;
+    std::vector<Worker*> worker{1};
+    int amountWorkers = 0;
+public:
+    Team(int inAmountWorkers): amountWorkers (inAmountWorkers)
+    {
+        assert (inAmountWorkers > 0);
+        for (int i = 0; i < inAmountWorkers; i++)
+        {
+            worker[i] = new Worker(this->getTask());
+            worker.push_back(worker[i]);
+        }
+    }
+
+    int getTask()
+    {
+        return 1 + std::rand() % (C - 1 + 1);
+    }
+
+};
+
 class Manager
 {
     int validTask = 0;
     int num = 0;
     int amountWorkers = 0;
-    Worker** workers = nullptr;
+    //Worker** workers = nullptr;
     int taskAmount = 0;
 public:
-    Manager (int inAmountWorkers, int inAmountTask): amountWorkers(inAmountWorkers), validTask(inAmountTask)
+    Manager (int inAmountTask, int inAmountWorkers):  validTask(inAmountTask), amountWorkers(inAmountWorkers)
     {
-        assert (inAmountWorkers > 0);
-        workers = new Worker*[inAmountWorkers];
+        assert (inAmountTask > 0);
         taskAmount = getAmountTask();
-        for (int i = 0; i < amountWorkers && i < taskAmount; i++)
-        {
-            workers[i] = new Worker(this->getTask());
-        }
+        Team* team = new Team(validTask);
     }
+
+
     int getAmountTask()
     {
         std::srand(validTask + num);
         return 1 + std::rand() % (amountWorkers - 1 + 1);
     }
-    int getTask()
+
+};
+
+class Head
+{
+    int teamAmount = 0;
+public:
+    Head(int inTeamAmount): teamAmount(inTeamAmount)
     {
-        return 1 + std::rand() % (C - 1 + 1);
+        assert (inTeamAmount > 0);
+        Manager **manager = nullptr;
+        manager = new Manager *[teamAmount];
+        for (int i = 0; i < teamAmount; i++) {
+
+            std::cout << "Team #" << i + 1 << std::endl;
+            std::cout << "Workers amount: ";
+            int workersAmount = 0;
+            std::cin >> workersAmount;
+            std::cout << "Boss task: ";
+            int bossTask = 0;
+            std::cin >> bossTask;
+            manager[i] = new Manager(workersAmount, bossTask);
+        }
     }
 };
 
@@ -61,18 +103,7 @@ int main() {
     int teamAmount;
     std::cout << "Team amount: ";
     std::cin >> teamAmount;
-    Manager** manager = nullptr;
-    manager = new Manager*[teamAmount];
-    for(int i = 0; i < teamAmount; i++)
-    {
-        std::cout << "Team #" << i + 1 << std::endl;
-        std::cout << "Workers amount: ";
-        int workersAmount = 0;
-        std::cin >> workersAmount;
-        std::cout << "Boss task: ";
-        int bossTask = 0;
-        std::cin >> bossTask;
-        manager[i] = new Manager(workersAmount, bossTask);
-    }
+    Head* header = new Head(teamAmount);
+
     return 0;
 }
